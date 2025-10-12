dccf1d6862cff82ec813371e6f925e86920ef5bf
karolnaze <w.karolnaze@gmail.com>
2025-10-13 00:11:42 +0200

    fenwick tre

--- Python/week-29092025/mergearrays.py
+++ Python/week-29092025/mergearrays.py
@@ -52,3 +52,16 @@ print("-------------------------------------------------------------")
 
 print(removeElement(nums = [1,2,3,5,7,9,2,2],val=2))
 
+#Remove duplicates from sorted array
+
+# nums = [1,1,2]
+# def removeDuplicates(nums):
+#     if not nums:
+#         return 0
+#     k = 1
+#     for i in range(1, len(nums)):
+#         if nums[i] != nums[i - 1]:
+#             nums[k] = nums[i]
+#             k += 1
+#     return k    
+
