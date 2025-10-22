1997b5101502ba4908b07ba7b34dc1f10ab83866
karolnaze <w.karolnaze@gmail.com>
2025-10-22 02:38:19 +0200

    stock_buy_and_sell - Algorithm

--- leetcode/buy_and_sell.py
+++ leetcode/buy_and_sell.py
@@ -1,7 +1,18 @@
-price = [5,6,7]
+prices = [7,1,5,3,6,4]
 
-buy_price = min(price)
-for i in range(price):
-    if price[i]==buy_price:
-        day = i
-profit = buy_price - price[day+1]
+left=0  #Buy --> Pointer --> I'm buying the stock
+right=1 #Sell --> Pointer --> I'm selling it 
+
+max_profit = 0 # The variable that contains the maximal profit
+
+while right < len(prices): # Going through the array in one time 
+    current_profit = prices[right] - prices[left] # The current profit -> The profit when i'm just going through the array
+    if prices[left]<prices[right]:  #The best condition for me 
+        max_profit = max(current_profit, max_profit)
+    else: 
+        left = right # I'm changing the position -> I'm looking for the next day to buy 
+    right+=1
+print(max_profit)
+
+
+
